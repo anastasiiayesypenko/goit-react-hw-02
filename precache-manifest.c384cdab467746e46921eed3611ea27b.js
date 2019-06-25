3526964203783357508096d888e9502186251295
Anastasia <nastiyayesypenko@ukr.net>
2019-06-25 13:38:32 +0300

    Updates

---
+++ precache-manifest.c384cdab467746e46921eed3611ea27b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81989faa392e7850dff94c2fff14af0",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "e716e2d45fb157a75d42",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.8c007e13.chunk.css"
+  },
+  {
+    "revision": "e1dd706ff3a8cecfe4db",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.c550fe45.chunk.js"
+  },
+  {
+    "revision": "e716e2d45fb157a75d42",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.1fb0cc08.chunk.js"
+  },
+  {
+    "revision": "1d8f9ed0b38c93c66413",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.d734538f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02/precache-manifest.f5140cb40874467ad84c3ed8a2610757.js"
+  "/goit-react-hw-03-bank-account/precache-manifest.c384cdab467746e46921eed3611ea27b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-02/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-03-bank-account/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
